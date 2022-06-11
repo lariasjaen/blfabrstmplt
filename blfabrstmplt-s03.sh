562d2d235479517310b6e5e2400c7be8da64d3e8
jonathanbrenes <77504106+jonathanbrenes@users.noreply.github.com>
2022-06-11 11:58:45 -0600

    Update blfabrstmplt-s03.sh

--- blfabrstmplt-s03.sh
+++ blfabrstmplt-s03.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 date > /tmp/blfabrstmplt-s03.txt
+echo Change ssh port >> /tmp/blfabrstmplt-s03.txt
 sed -Ei 's/^#?([[:space:]]?)+Port([[:space:]]?22([[:space:]]?)+)+$/Port 2222/' /etc/ssh/sshd_config
+echo Change selinux ssh port >> /tmp/blfabrstmplt-s03.txt
 semanage port -a -t ssh_port_t -p tcp 2222
+echo Add firewall ssh port >> /tmp/blfabrstmplt-s03.txt
 firewall-cmd --zone=public --add-port=2222/tcp --permanent
-sleep 2
+echo Reload firewall >> /tmp/blfabrstmplt-s03.txt
 firewall-offline-cmd --reload
+echo Restart ssh >> /tmp/blfabrstmplt-s03.txt
 systemctl restart sshd
 date >> /tmp/blfabrstmplt-s03.txt

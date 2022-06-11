46d8f79bf398341b4c01226ff741c1a11157e1b6
jonathanbrenes <77504106+jonathanbrenes@users.noreply.github.com>
2022-06-11 11:24:03 -0600

    Update blfabrstmplt-s03.sh

--- blfabrstmplt-s03.sh
+++ blfabrstmplt-s03.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+date > /tmp/blfabrstmplt-s03.txt
 sed -Ei 's/^#?([[:space:]]?)+Port([[:space:]]?22([[:space:]]?)+)+$/Port 2222/' /etc/ssh/sshd_config
 semanage port -a -t ssh_port_t -p tcp 2222
 firewall-cmd --zone=public --add-port=2222/tcp --permanent
 sleep 2
 firewall-cmd --reload
 systemctl restart sshd
+date >> /tmp/blfabrstmplt-s03.txt

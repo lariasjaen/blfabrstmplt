c3f3e5a3d9a647be246c2b74d9e8ff48fe9d72cc
jonathanbrenes <77504106+jonathanbrenes@users.noreply.github.com>
2022-06-11 12:38:16 -0600

    Update blfabrstmplt-s03.sh

--- blfabrstmplt-s03.sh
+++ blfabrstmplt-s03.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 date > /tmp/blfabrstmplt-s03.txt
-echo Change ssh port >> /tmp/blfabrstmplt-s03.txt
-sed -Ei 's/^#?([[:space:]]?)+Port([[:space:]]?22([[:space:]]?)+)+$/Port 2222/' /etc/ssh/sshd_config
+echo Change sshd port configuration >> /tmp/blfabrstmplt-s03.txt
+sed -Ei 's/^#?([[:space:]]?)+Port([[:space:]]?22([[:space:]]?)+)+$/Port 2222 # Modified by automation/' /etc/ssh/sshd_config 2>&1 >>  /tmp/blfabrstmplt-s03.txt
 
-echo Change selinux ssh port >> /tmp/blfabrstmplt-s03.txt
-semanage port -a -t ssh_port_t -p tcp 2222
+echo Add TCP port 2222 to SELinux >> /tmp/blfabrstmplt-s03.txt
+semanage port -a -t ssh_port_t -p tcp 2222 >> /tmp/blfabrstmplt-s03.txt
 
 echo Add firewall ssh port >> /tmp/blfabrstmplt-s03.txt
-systemctl stop firewalld
-firewall-offline-cmd --add-port=2222/tcp --permanent
-
-echo Start firewall >> /tmp/blfabrstmplt-s03.txt
-systemctl start firewalld
+systemctl stop firewalld >> /tmp/blfabrstmplt-s03.txt
+firewall-offline-cmd --add-port=2222/tcp --permanent >> /tmp/blfabrstmplt-s03.txt
+systemctl start firewalld >> /tmp/blfabrstmplt-s03.txt
 
 echo Restart ssh >> /tmp/blfabrstmplt-s03.txt
-systemctl restart sshd
+systemctl restart sshd >> /tmp/blfabrstmplt-s03.txt
 
 date >> /tmp/blfabrstmplt-s03.txt
